927881c5948e1cd08c1263f613f79b2103c420ae
GhostMaster69-dev <rathore6375@gmail.com>
2022-01-16 02:24:59 +0000

    Revert "SystemUI: Pulse edge light for all doze triggers [1/2]"
    This reverts commit e697d5e52c5599336f30b5f8ec6e4e8171ec2b55.
    
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelViewController.java
@@ -3828,8 +3828,6 @@ public class NotificationPanelViewController extends PanelViewController {
                 Settings.System.AOD_NOTIFICATION_PULSE_TIMEOUT, 0, UserHandle.USER_CURRENT);
         boolean pulseColorAutomatic = Settings.System.getIntForUser(resolver,
                 Settings.System.NOTIFICATION_PULSE_COLOR_MODE, 0, UserHandle.USER_CURRENT) == 2;
-        boolean pulseForAll = Settings.System.getIntForUser(resolver,
-                Settings.System.AMBIENT_LIGHT_PULSE_FOR_ALL, 0, UserHandle.USER_CURRENT) == 1;
         int repeats = Settings.System.getIntForUser(resolver,
                 Settings.System.NOTIFICATION_PULSE_REPEATS, 0, UserHandle.USER_CURRENT);
         if (animatePulse) {
@@ -3862,7 +3860,7 @@ public class NotificationPanelViewController extends PanelViewController {
                 }
             }
             if (mPulsing) {
-                if (pulseReasonNotification || pulseForAll) {
+                if (pulseReasonNotification) {
                     if (activeNotif) {
                         // show the bars if we have to
                         if (pulseLights) {
